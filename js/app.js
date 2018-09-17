a3d12844ff21bd5ec2180f04148e49f1c0a4b6c3
Reuben Ayres <ra@sent.com>
2018-09-17 13:55:17 -0500

    pseudocoded rwo2

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 console.log("hey");
+
+const reverseWordOrder2 = (str) => {
+    // build output string as you go
+
+    // keep track of current "word"
+
+    // loop over digits in string 
+
+        // when you get to a space 
+
+            // a space should be added in the proper place
+
+            // the current word is "done" and should be added to the string in a proper place
+
+            // clear out the "current word"
+
+}
+            
+const reversed = reverseWordOrder2("Four score and seven years ago")
+console.log(reversed)
